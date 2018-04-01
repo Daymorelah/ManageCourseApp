5d51acf721759470f962ae2b5a2980289e1ed2e9
--replace-all <demola.hussainin@gmail.com>
2018-04-01 04:18:30 +0100

    make already registeed cousres show when updating a course

--- src/Components/Courses/manageCoursePage.jsx
+++ src/Components/Courses/manageCoursePage.jsx
@@ -15,6 +15,11 @@ class ManageCoursePage extends Component {
     this.updateCourseState = this.updateCourseState.bind(this);
     this.saveCourse = this.saveCourse.bind(this);
   }
+  componentWillReceiveProps(nextProps){ //This life cycle emthod get called by react if react thinks props
+    if(this.props.course.id != nextProps.course.id){ //has changed with new data
+      this.setState({course: Object.assign({}, nextProps.course)})
+    }
+  }
   updateCourseState(event){
     const field = event.target.name;
     let course = this.state.course;
@@ -42,14 +47,24 @@ ManageCoursePage.propTypes = {
   actions: PropTypes.object.isRequired
 }
 
-ManageCoursePage.contextTypes = {
-  router: PropTypes.object
+// ManageCoursePage.contextTypes = {
+//   router: PropTypes.object
+// } check contextTypes on react for more info. history.push does d same work so used it instead.
+
+function getCourseById(courses, id){
+  const course = courses.filter( course => course.id == id ); //note filter returns an array.
+  if (course) return course[0]; // so we return the first occurance.
+  return null;
 }
 
 function mapStateToProps (state, ownProps){
+  const courseId = ownProps.match.params.id; //cos we used 'id' in the route i.e. '/course:id'
   let course = { id:'', watchHref:'', title:'', authorId:'', length:'', category:'' };
   //lets format the author data coming form the API so dt select form type can use it. since we created
   //selectInput before working on the author data.
+  if(courseId && state.courses.length > 0){
+    course = getCourseById(state.courses, courseId);
+  }
   const formatedAuthorDataForSelectInputField = state.authors.map( author => {
     return {
       value: author.id,  
