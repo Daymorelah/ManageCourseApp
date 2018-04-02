29b431186ba1032222b5362ba2701cbae7b35673
--replace-all <demola.hussainin@gmail.com>
2018-04-02 11:59:16 +0100

    include testing framework

---
+++ src/Components/Courses/courseForm.test.js
@@ -0,0 +1,14 @@
+import expect from 'expect';
+import React from 'react';
+import testUtils from 'react-addons-test-utils';
+import CourseForm from './courseForm.jsx';
+
+function setUp(){
+  let 
+}
+
+describe('CourseForm via react test utils', () =>{
+  it('renders form and h1', () =>{
+
+  })
+})
